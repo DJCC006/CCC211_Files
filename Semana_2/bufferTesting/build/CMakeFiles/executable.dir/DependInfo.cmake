6ec94079f009b43df6ba96eae677739a8247c1c0
DJCC <david.carcamo2006@gmail.com>
2026-07-31 01:47:11 -0600

    trabajos realizados hasta semana 2

---
+++ Semana_2/bufferTesting/build/CMakeFiles/executable.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/david/Documents/Universidad/CCC211/Semana_2/bufferTesting/Buffer.cpp" "CMakeFiles/executable.dir/Buffer.cpp.o" "gcc" "CMakeFiles/executable.dir/Buffer.cpp.o.d"
+  "/home/david/Documents/Universidad/CCC211/Semana_2/bufferTesting/FileWrite2.cpp" "CMakeFiles/executable.dir/FileWrite2.cpp.o" "gcc" "CMakeFiles/executable.dir/FileWrite2.cpp.o.d"
+  "" "executable" "gcc" "CMakeFiles/executable.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
